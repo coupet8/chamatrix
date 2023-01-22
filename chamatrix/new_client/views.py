26bf76524e383ec52e3e697bc6d349dd790af478
Anton Kovalev <kovalev8992@gmail.com>
2023-01-22 23:23:39 +0300

    Изменены приложения new_client и generation

--- chamatrix/new_client/views.py
+++ chamatrix/new_client/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello")
+
+def send(request):
+    return HttpResponse("Отправить")
+
+def generate(request):
+    return HttpResponse("Генерировать")
