9332f488f3a6b0078020a1ee99db185229332455
Anton Kovalev <kovalev8992@gmail.com>
2023-01-23 18:23:07 +0300

    Изменения в БД, новый файл calculation

--- chamatrix/new_client/models.py
+++ chamatrix/new_client/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255)
+    birthday = models.DateField()
+    year = models.IntegerField()
