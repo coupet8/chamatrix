4e59821ea44b9fa660a86c8087926d193efbd4d5
Anton Kovalev <kovalev8992@gmail.com>
2023-02-14 00:22:43 +0300

    Добил скрипт замены текста в шаблоне на нужный

--- chamatrix/new_client/personal_matrix.py
+++ chamatrix/new_client/personal_matrix.py
@@ -35,45 +35,25 @@ a_n = str((read_single_row(matrix.a_n))[9])
 b_n = str((read_single_row(matrix.b_n))[10])
 e_n = str((read_single_row(matrix.e_n))[5])
 
-def replace_string(filename):
-    document = docx.Document(filename)
+document = docx.Document("your_matrix_template.docx")
+def replace_text(old_text, new_text):
     for paragraph in document.paragraphs:
-        if '{ c_l_intro }' in paragraph.text:
-            inline = paragraph.runs
-            for i in range(len(inline)):
-                if '{ c_l_intro }' in inline[i].text:
-                    text = inline[i].text.replace("{ c_l_intro }", str(c_l_intro))
-                    inline[i].text = text
+        if old_text in paragraph.text:
+            paragraph.text = paragraph.text.replace(old_text, new_text)
 
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace('{ c_l_tasks }', str(c_l_tasks))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_l_task }", str(d_l_task))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_l_minus }", str(d_l_minus))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_l_results }", str(d_l_results)) 
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ e_l }", str(e_l))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ c_n_intro }", str(c_n_intro))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ c_n_tasks }", str(c_n_tasks))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_n_task }", str(d_n_task))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_n_intro }", str(d_n_intro))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_n_plus }", str(d_n_plus))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ d_n_conclusion }", str(d_n_conclusion))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ a_n }", str(a_n))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ b_n }", str(b_n))
-#for paragraph in document.paragraphs:
-    #paragraph.text = paragraph.text.replace("{ e_n }", str(e_n))
-
-    document.save("your_matrix_1.docx")
-    return 1
-replace_string("your_matrix_template.docx")
+replace_text("{ c_l_intro }", str(c_l_intro))
+replace_text("{ c_l_tasks }", str(c_l_tasks))
+replace_text("{ d_l_task }", str(d_l_task))
+replace_text("{ d_l_minus }", str(d_l_minus))
+replace_text("{ d_l_results }", str(d_l_results))
+replace_text("{ e_l }", str(e_l))
+replace_text("{ c_n_intro }", str(c_n_intro))
+replace_text("{ c_n_tasks }", str(c_n_tasks))
+replace_text("{ d_n_task }", str(d_n_task))
+replace_text("{ d_n_intro }", str(d_n_intro))
+replace_text("{ d_n_plus }", str(d_n_plus))
+replace_text("{ d_n_conclusion }", str(d_n_conclusion))
+replace_text("{ a_n }", str(a_n))
+replace_text("{ b_n }", str(b_n))
+replace_text("{ e_n }", str(e_n))
+document.save("your_matrix_1.docx")
