913eaf869cab233eeb388e7280e816e50de262ea
Anton Kovalev <kovalev8992@gmail.com>
2023-02-16 16:31:19 +0300

    eqwq

--- chamatrix/chamatrix/settings.py
+++ chamatrix/chamatrix/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'chamatrix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['new_client'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- chamatrix/chamatrix/urls.py
+++ chamatrix/chamatrix/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     #path('register/', user_views.register, name='register'), отключаем на время
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('home/', views.home, name='home'),  
+    path('home/', views.home, name='home'),
 ]
